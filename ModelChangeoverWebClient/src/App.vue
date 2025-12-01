e7269c8d511cfeeddcca96ce553068783c11a997
Rogelio Arriaga <r.arriaga@generaltransmissions.com>
2025-12-01 16:09:27 -0600

    Restart Program Change to svelte kit and add tailwind

--- ModelChangeoverWebClient/src/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="min-h-screen flex items-center justify-center bg-slate-900">
-    <h1 class="text-3xl font-bold text-white">
-      Tailwind v4 + Vue funcionando 😎
-    </h1>
-  </div>
-</template>
